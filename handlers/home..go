ea26ee8bddf96f9eccad47126aae4d5a3148eccf
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-08-19 19:02:50 +0530

    fix/foo-to-home-transition

--- handlers/home..go
+++ handlers/home..go
@@ -3,9 +3,9 @@ package handler
 import (
 	"net/http"
 
-	foo "github.com/arnavmahajan630/GOTTH-TEMPLATE/views"
+	"github.com/arnavmahajan630/GOTTH-TEMPLATE/views/home"
 )
 
 func Foo(w http.ResponseWriter, r * http.Request) error {
-	return Render(w, r, foo.Index())
+	return Render(w, r, home.Index())
 }
